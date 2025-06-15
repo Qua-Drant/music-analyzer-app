2b3dd695372c559e9c493c349d2e7211a1ecfa86
千城暮雪 <fengyj33@gmail.com>
2025-06-15 12:48:22 +0800

    Add Vite config and GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/<music-analyzer-app>/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
